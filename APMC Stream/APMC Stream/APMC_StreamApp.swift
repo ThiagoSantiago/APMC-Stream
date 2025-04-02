d51832f4ae0358f61a00aea83565afd10702b5b3
Thiago Santiago <thiagoalexandres@gmail.com>
2025-04-02 13:56:58 -0300

    Create Project

---
+++ APMC Stream/APMC Stream/APMC_StreamApp.swift
@@ -0,0 +1,17 @@
+//
+//  APMC_StreamApp.swift
+//  APMC Stream
+//
+//  Created by Thiago Santiago on 02/04/25.
+//
+
+import SwiftUI
+
+@main
+struct APMC_StreamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
